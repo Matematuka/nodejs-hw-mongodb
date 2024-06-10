38f58970ead4ff2406c500d48e92dec1115046d2
Matematuka <143654489+Matematuka@users.noreply.github.com>
2024-06-10 14:20:03 +0300

    post/patch validation

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -3,9 +3,9 @@ import Joi from 'joi';
 export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
   phoneNumber: Joi.string().min(5).max(20).required(),
-  email: Joi.string().email().required(),
+  email: Joi.string().email(),
   isFavourite: Joi.boolean(),
-  contactType: Joi.string().valid('work', 'home', 'personal').required(),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
 });
 
 export const updateContactSchema = Joi.object({
@@ -13,5 +13,5 @@ export const updateContactSchema = Joi.object({
   phoneNumber: Joi.string().min(5).max(20),
   email: Joi.string().email(),
   isFavourite: Joi.boolean(),
-  contactType: Joi.string().valid('work', 'home', 'personal').required(),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
 });
