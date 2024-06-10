0026424e56eca0c87be28a4c3eb85a8df2273321
Matematuka <143654489+Matematuka@users.noreply.github.com>
2024-06-10 13:09:10 +0300

    sort and filter

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,29 +1,39 @@
 import { contactsCollection } from '../db/models/contact.js';
 import { calculatePaginationData } from '../utils/calculatePaginationData.js';
+import { SORT_ORDER } from '../constants/index.js';
 
-export const getContacts = async ({ page, perPage }) => {
+export const getContacts = async ({
+  page = 1,
+  perPage = 10,
+  sortOrder = SORT_ORDER.ASC,
+  sortBy = 'name',
+  filter = {},
+}) => {
   const limit = perPage;
   const skip = (page - 1) * perPage;
-  try {
-    const contactsQuery = contactsCollection.find();
-    const contactsCount = await contactsCollection
-      .find()
-      .merge(contactsQuery)
-      .countDocuments();
-    const contacts = await contactsQuery.skip(skip).limit(limit).exec();
 
-    const paginationData = calculatePaginationData(
-      contactsCount,
-      perPage,
-      page,
-    );
-    return {
-      data: contacts,
-      ...paginationData,
-    };
-  } catch (error) {
-    console.log(error);
+  const contactsQuery = contactsCollection.find();
+  if (filter.contactType) {
+    contactsQuery.where('contactType').equals(filter.contactType);
   }
+  if (filter.isFavourite) {
+    contactsQuery.where('isFavourite').equals(filter.isFavourite);
+  }
+
+  const [contactsCount, contacts] = await Promise.all([
+    contactsCollection.find().merge(contactsQuery).countDocuments(),
+    contactsQuery
+      .skip(skip)
+      .limit(limit)
+      .sort({ [sortBy]: sortOrder })
+      .exec(),
+  ]);
+
+  const paginationData = calculatePaginationData(page, perPage, contactsCount);
+  return {
+    data: contacts,
+    ...paginationData,
+  };
 };
 
 export const getContactByID = async (id) => {
