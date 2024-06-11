eb098fc11d83299d18947e42503dc9b9174b3af4
Matematuka <143654489+Matematuka@users.noreply.github.com>
2024-06-11 17:12:13 +0300

    hash

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,5 +1,11 @@
+import bcrypt from 'bcrypt';
 import { UsersCollection } from '../db/models/user.js';
 
 export const registerUser = async (payload) => {
-  return await UsersCollection.create(payload);
+  const encryptedPassword = await bcrypt.hash(payload.password, 10);
+
+  return await UsersCollection.create({
+    ...payload,
+    password: encryptedPassword,
+  });
 };
