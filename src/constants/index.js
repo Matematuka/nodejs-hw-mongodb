53311304526f1afd989950e0a43abb7f33ecf112
Matematuka <143654489+Matematuka@users.noreply.github.com>
2024-06-29 14:18:03 +0300

    saveImage

--- src/constants/index.js
+++ src/constants/index.js
@@ -15,6 +15,12 @@ export const SORT_ORDER = {
   DESC: 'desc',
 };
 
+export const CLOUDINARY = {
+  CLOUD_NAME: 'CLOUD_NAME',
+  API_KEY: 'API_KEY',
+  API_SECRET: 'API_SECRET',
+};
+
 export const FIFTEEN_MINUTES = 15 * 60 * 1000;
 export const ONE_MONTH = 30 * 24 * 60 * 60 * 1000;
 
@@ -27,3 +33,6 @@ export const SMTP = {
 };
 
 export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
+
+export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
+export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
